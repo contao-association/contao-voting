cc97dff63e97371798390c4a4f759c5e2710f184
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-11-27 22:21:50 +0100

    Added german language files

---
+++ languages/de/default.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * voting extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2008-2014, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       http://github.com/contao-association/contao-voting
+ */
+
+/**
+ * Miscellaneous
+ */
+$GLOBALS['TL_LANG']['MSC']['voting_vote']                   = 'Abstimmen';
+$GLOBALS['TL_LANG']['MSC']['voting_summary']                = 'Abstimmungs-Zusammenfassung';
+$GLOBALS['TL_LANG']['MSC']['voting_total']                  = 'Anzahl Stimmen';
+$GLOBALS['TL_LANG']['MSC']['voting_options']['yes']         = 'Ja';
+$GLOBALS['TL_LANG']['MSC']['voting_options']['no']          = 'Nein';
+$GLOBALS['TL_LANG']['MSC']['voting_options']['abstention']  = 'Stimmenthaltung';
+
+$GLOBALS['TL_LANG']['MSC']['hasVoted'] = 'Du hast an dieser Abstimmung bereits teilgenommen.';
