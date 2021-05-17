4783e232efca9183e7f4abdce34e152f06d47390
Andreas Schempp <andreas.schempp@terminal42.ch>
2021-05-17 09:58:59 +0200

    Moved template labels to new language file

--- contao/languages/de/default.php
+++ contao/languages/de/default.php
@@ -1,15 +1,6 @@
 <?php
 
 /**
- * voting extension for Contao Open Source CMS
- *
- * @copyright  Copyright (c) 2008-2014, terminal42 gmbh
- * @author     terminal42 gmbh <info@terminal42.ch>
- * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
- * @link       http://github.com/contao-association/contao-voting
- */
-
-/**
  * Miscellaneous
  */
 $GLOBALS['TL_LANG']['MSC']['voting_vote']                   = 'Abstimmen';
@@ -19,5 +10,3 @@ $GLOBALS['TL_LANG']['MSC']['voting_participants']           = 'Anzahl Teilnehmer
 $GLOBALS['TL_LANG']['MSC']['voting_options']['yes']         = 'Ja';
 $GLOBALS['TL_LANG']['MSC']['voting_options']['no']          = 'Nein';
 $GLOBALS['TL_LANG']['MSC']['voting_options']['abstention']  = 'Stimmenthaltung';
-
-$GLOBALS['TL_LANG']['MSC']['hasVoted'] = 'Du hast an dieser Abstimmung bereits teilgenommen.';
